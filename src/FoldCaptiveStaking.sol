60ec6a2809a41b170aceaccaa341503d914020fa
ControlCplusControlV <randomlikeabledude@gmail.com>
2024-07-03 00:52:30 -0700

    more fixes

--- src/FoldCaptiveStaking.sol
+++ src/FoldCaptiveStaking.sol
@@ -21,6 +21,7 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
     /*//////////////////////////////////////////////////////////////
                              INITIALIZATION
     //////////////////////////////////////////////////////////////*/
+    bool public initialized;
 
     /// @param _positionManager The Canonical UniswapV3 PositionManager
     /// @param _pool The FOLD Pool to Reward
@@ -30,22 +31,26 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
         positionManager = INonfungiblePositionManager(_positionManager);
         POOL = IUniswapV3Pool(_pool);
 
-        token1 = ERC20(POOL.token1());
         token0 = ERC20(POOL.token0());
+        token1 = ERC20(POOL.token1());
 
         WETH = WETH9(payable(_weth));
         FOLD = ERC20(_fold);
 
+        initialized = false;
+    }
+
+    function initialize() public {
         // We must mint the pool a small dust LP position, which also prevents share attacks
         // So this is our "minimum shares"
         INonfungiblePositionManager.MintParams memory params = INonfungiblePositionManager.MintParams({
             token0: address(token0),
             token1: address(token1),
             fee: 10000,
-            tickLower: TICK_LOWER,
-            tickUpper: TICK_UPPER,
-            amount0Desired: 10_000,
-            amount1Desired: 10_000,
+            tickLower: -887_200,
+            tickUpper: 887_200,
+            amount0Desired: 1_000_000,
+            amount1Desired: 1_000_000,
             amount0Min: 0,
             amount1Min: 0,
             recipient: address(this),
@@ -55,14 +60,18 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
         token0.approve(address(positionManager), type(uint256).max);
         token1.approve(address(positionManager), type(uint256).max);
 
-        WETH.approve(address(positionManager), type(uint256).max);
-        FOLD.approve(address(positionManager), type(uint256).max);
-
         uint128 liquidity;
         (TOKEN_ID, liquidity,,) = positionManager.mint(params);
         require(liquidity > 0, "ZERO Liquidity");
 
         liquidityUnderManagement += uint256(liquidity);
+        
+        initialized = true;
+    }
+
+    modifier isInitialized {
+      require(initialized, "NO INIT");
+      _;
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -82,7 +91,7 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
     /// @dev token1 in terms of the Uniswap Pool
     ERC20 public immutable token1;
     /// @dev The tokenId of the UniswapV3 position
-    uint256 public immutable TOKEN_ID;
+    uint256 public TOKEN_ID;
     /// @dev Used for all rewards related tracking
     uint256 public liquidityUnderManagement;
     /// @dev Used to keep track of rewards given per share
@@ -110,7 +119,7 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
     ERC20 public immutable FOLD;
 
     /// @notice Allows anyone to add funds to the contract, split among all depositors
-    function depositRewards() payable public {
+    function depositRewards() isInitialized payable public {
       WETH.deposit{value: msg.value}();
       rewardsPerLiquidity += msg.value;
     }
@@ -122,7 +131,7 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
     /// @param amount0 The amount of token0 to deposit 
     /// @param amount1 The amount of token1 to deposit
     /// @param slippage Slippage on deposit out of 1e18
-    function deposit(uint256 amount0, uint256 amount1, uint256 slippage) external {
+    function deposit(uint256 amount0, uint256 amount1, uint256 slippage) isInitialized external {
         collectFees();
         collectRewards();
 
@@ -153,7 +162,7 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
     }
 
     /// @notice Compounds User Earned Fees back into their position 
-    function compound() public {
+    function compound() isInitialized public {
         collectPositionFees();
 
         uint256 fee0Owed = (token0FeesPerLiquidity - balances[msg.sender].token0FeeDebt) * balances[msg.sender].amount
@@ -184,7 +193,7 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
     }
 
     /// @notice User-specific function to collect fees on the singular position
-    function collectFees() public {
+    function collectFees() isInitialized public {
         collectPositionFees();
 
         uint256 fee0Owed = (token0FeesPerLiquidity - balances[msg.sender].token0FeeDebt) * balances[msg.sender].amount
@@ -200,7 +209,7 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
     }
 
     /// @notice User-specific Rewards for Protocol Rewards
-    function collectRewards() public {
+    function collectRewards() isInitialized public {
         uint256 rewardsOwed = (rewardsPerLiquidity - balances[msg.sender].rewardDebt) * balances[msg.sender].amount
             / liquidityUnderManagement;
         WETH.transfer(msg.sender, rewardsOwed);
@@ -209,14 +218,14 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
     }
 
     /// @notice liquidity The Liquidity Value which the user wants to withdraw
-    function withdraw(uint128 liquidity) external {
+    function withdraw(uint128 liquidity) isInitialized external {
         collectFees();
         collectRewards();
 
         balances[msg.sender].amount -= liquidity;
-        liquidityUnderManagement += uint256(liquidity);
+        liquidityUnderManagement -= uint256(liquidity);
 
-        INonfungiblePositionManager.DecreaseLiquidityParams memory params = INonfungiblePositionManager
+        INonfungiblePositionManager.DecreaseLiquidityParams memory decreaseParams = INonfungiblePositionManager
             .DecreaseLiquidityParams({
             tokenId: TOKEN_ID,
             liquidity: liquidity,
@@ -225,7 +234,18 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
             deadline: block.timestamp + 1 minutes
         });
 
-        (uint256 amount0, uint256 amount1) = positionManager.decreaseLiquidity(params);
+        (uint256 amount0, uint256 amount1) = positionManager.decreaseLiquidity(decreaseParams);
+
+        INonfungiblePositionManager.CollectParams memory collectParams = INonfungiblePositionManager.CollectParams({
+            tokenId: TOKEN_ID,
+            recipient: address(this),
+            amount0Max: uint128(amount0),
+            amount1Max: uint128(amount1)
+        });
+
+        (uint256 amount0Collected, uint256 amount1Collected) = positionManager.collect(collectParams);
+
+        require(amount0Collected == amount0 && amount1Collected == amount1, "WITHDRAW: FAIL");
 
         token0.transfer(msg.sender, amount0);
         token1.transfer(msg.sender, amount1);
---
+++ test/mock/MockERC20.sol
@@ -0,0 +1,15 @@
+/// SPDX-License-Identifier: SSPL-1.-0
+pragma solidity 0.8.25;
+
+import { ERC20 } from "lib/solmate/src/tokens/ERC20.sol";
+
+
+contract MockERC20 is ERC20("Manifold Finance", "FOLD", 18) {
+  function mint(address recipient, uint256 amount) public {
+    _mint(recipient, amount);
+  }
+
+  function burn(address recipient, uint256 amount) public {
+    _burn(recipient, amount);
+  }
+}
