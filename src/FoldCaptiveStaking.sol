dcb984fc78b6130d960e94221f9121659d43aae5
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2024-07-04 00:45:19 -0600

    Copypaste (#3)
    * simplify
    
    * oops legal
    
    * added deposit func
    
    * added in a fee compound function
    
    * more fixes
    
    * much better testing

--- src/FoldCaptiveStaking.sol
+++ src/FoldCaptiveStaking.sol
@@ -198,8 +198,9 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
 
         uint256 fee0Owed = (token0FeesPerLiquidity - balances[msg.sender].token0FeeDebt) * balances[msg.sender].amount
             / liquidityUnderManagement;
-        uint256 fee1Owed = token1FeesPerLiquidity
-            - balances[msg.sender].token1FeeDebt * balances[msg.sender].amount / liquidityUnderManagement;
+        uint256 fee1Owed = (token1FeesPerLiquidity
+            - balances[msg.sender].token1FeeDebt) * balances[msg.sender].amount / liquidityUnderManagement;
+
 
         token0.transfer(msg.sender, fee0Owed);
         token1.transfer(msg.sender, fee1Owed);
@@ -212,6 +213,7 @@ contract FoldCaptiveStaking is Owned(msg.sender) {
     function collectRewards() isInitialized public {
         uint256 rewardsOwed = (rewardsPerLiquidity - balances[msg.sender].rewardDebt) * balances[msg.sender].amount
             / liquidityUnderManagement;
+            
         WETH.transfer(msg.sender, rewardsOwed);
 
         balances[msg.sender].rewardDebt = uint128(rewardsPerLiquidity);
